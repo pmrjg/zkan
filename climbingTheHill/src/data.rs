e156ec416375e170990b92df19091e86ebf23e7e
Pedro Jorge <pmrjge@gmail.com>
2024-12-24 22:27:20 +0000

    Fixed a few bugs

--- climbingTheHill/src/data.rs
+++ climbingTheHill/src/data.rs
@@ -2,21 +2,33 @@ use std::sync::Arc;
 use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
 use vulkano::VulkanLibrary;
 use pub_fields::pub_fields;
-use vulkano::device::DeviceExtensions;
+use vulkano::device::{DeviceExtensions, QueueFlags};
 use vulkano::device::physical::{PhysicalDevice, PhysicalDeviceType};
 use vulkano::swapchain::Surface;
+use winit::window::Window;
+use winit::event_loop::EventLoop;
 
 #[pub_fields]
 pub struct EngineData{
     vk_library: Arc<VulkanLibrary>,
     vk_instance: Arc<Instance>,
+    event_loop: Arc<EventLoop<()>>,
+    window: Arc<Window>,
+    surface: Arc<Surface>,
+    physical_device: Arc<PhysicalDevice>,
 }
 impl EngineData {
     pub fn new() -> Self {
-        let vulkan_library = Self::get_vk_library();
-        let vk_instance = Self::get_vk_instance(vulkan_library.clone());
-
-        Self {vk_library, vk_instance}
+        let vk_library = Self::get_vk_library();
+        let vk_instance = Self::get_vk_instance(vk_library.clone());
+        let devices = Self::get_vk_physical_devices(vk_instance.clone());
+        let event_loop = Arc::new(EventLoop::new().unwrap());
+        #[allow(deprecated)]
+        let window =  Arc::new(event_loop.create_window(Window::default_attributes()).unwrap());
+        let surface = Surface::from_window(vk_instance.clone(), window.clone()).unwrap();
+        
+        let physical_device = Self::pick_physical_device(&devices,  surface.clone());
+        Self {vk_library, vk_instance, event_loop, window, surface, physical_device}
     }
 
     fn get_vk_library() -> Arc<VulkanLibrary> {
@@ -30,22 +42,23 @@ impl EngineData {
         }).expect("failed to create instance")
     }
 
-    fn get_vk_physical_devices(instance: Arc<Instance>) -> Arc<impl ExactSizeIterator<Item = Arc<PhysicalDevice>>>{
-        instance.enumerate_physical_devices().expect("failed to enumerate physical devices")
+    fn get_vk_physical_devices(instance: Arc<Instance>) -> Vec<Arc<PhysicalDevice>> {
+        let x = instance.enumerate_physical_devices().expect("failed to enumerate physical devices");
+        Vec::from_iter(x)
     }
-
-    fn pick_physical_device(devices: &mut dyn ExactSizeIterator<Item = Arc<PhysicalDevice>>, surface: Arc<Surface>) -> Arc<PhysicalDevice> {
+    
+    fn pick_physical_device(devices: &Vec<Arc<PhysicalDevice>>, surface: Arc<Surface>) -> Arc<PhysicalDevice> {
         let device_extensions = DeviceExtensions {
             khr_swapchain: true,
             ..DeviceExtensions::empty()
         };
-        devices.filter(|device| device.supported_extensions().contains(&device_extensions))
+        devices.iter().filter(|device| device.supported_extensions().contains(&device_extensions))
             .filter_map(|device| {
                 device.queue_family_properties()
                     .iter()
                     .enumerate()
-                    .position(|i, d| {
-                    d.queue_flags.graphics && device.surface_support(u32::from(i), &surface).unwrap_or(false)
+                    .position(|(i, d)| {
+                    d.queue_flags.contains(QueueFlags::GRAPHICS) && device.surface_support(i as u32, &surface).unwrap_or(false)
                 }).map(|i| (device, i as u32))
             }).min_by_key(|(p, _)| {
             match p.properties().device_type {
@@ -56,6 +69,6 @@ impl EngineData {
                 PhysicalDeviceType::Other => 4,
                 _ => 5
             }
-        }).expect("No suitable physical device found").0
+        }).expect("No suitable physical device found").0.clone()
     }
 }
