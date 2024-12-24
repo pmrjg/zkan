a4cfe7c75c5b3102ae88206c9257eb4d2408dad9
Pedro Jorge <pmrjge@gmail.com>
2024-12-24 12:27:37 +0000

    Rust:: physical device search

--- rust-idiom/src/main.rs
+++ rust-idiom/src/main.rs
@@ -20,7 +20,7 @@ fn main() {
     let instance = {
         let library = VulkanLibrary::new().unwrap();
         let extensions = vulkano_win::required_extensions(&library);
-        
+
         Instance::new(
             library,
             InstanceCreateInfo {
@@ -31,4 +31,34 @@ fn main() {
             }
         ).unwrap()
     };
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new().build_vk_surface(&event_loop, instance.clone()).unwrap();
+
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::empty()
+    };
+
+    let (physical_device, queue_family_index) = instance.enumerate_physical_devices().unwrap()
+        .filter(|p| p.supported_extensions().contains(&device_extensions))
+        .filter_map(|p| {
+            p.queue_family_properties()
+                .iter()
+                .enumerate()
+                .position(|(i, q)| {
+                    q.queue_flags.graphics && p.surface_support(i as u32, &surface).unwrap_or(false)
+                })
+                .map(|i| (p, i as u32))
+        })
+        .min_by_key(|(p, _)| {
+            match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5
+            }
+        }).expect("No suitable physical device found");
 }
