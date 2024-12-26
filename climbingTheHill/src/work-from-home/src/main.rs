7b588e7381e08274f42abada408a4f12a3d268b6
Pedro Jorge <pmrjge@gmail.com>
2024-12-26 12:54:58 +0000

    Just for luck

--- climbingTheHill/src/work-from-home/src/main.rs
+++ climbingTheHill/src/work-from-home/src/main.rs
@@ -1,5 +1,8 @@
 use climbing_the_hill::e_computing::EngineComputing;
-use vulkano::buffer::BufferContents;
+use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
+use vulkano::format::Format;
+use vulkano::image::view::ImageView;
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter};
 use vulkano::pipeline::graphics::vertex_input::Vertex;
 
 #[derive(BufferContents, Vertex)]
@@ -32,5 +35,42 @@ fn main() {
         db
     } = proc;
 
-   
+    let v1 = PVertex{ position: [-0.5, -0.5] };
+    let v2 = PVertex{ position: [ 0.0, 0.5] };
+    let v3 = PVertex{ position: [ 0.5, -0.25] };
+    
+    let vertex_buffer = Buffer::from_iter(
+      memory_allocator.clone(),
+      BufferCreateInfo {
+          usage: BufferUsage::VERTEX_BUFFER,
+          ..Default::default()
+      },
+      AllocationCreateInfo {
+          memory_type_filter: MemoryTypeFilter::PREFER_DEVICE | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+          ..Default::default()
+      },
+      vec![v1, v2, v3]
+    ).unwrap();
+    
+    // Vertex Shader
+    
+    
+    // Render pass
+    let render_pass = vulkano::single_pass_renderpass!(
+    logical_device.clone(),
+    attachments: {
+        color: {
+            format: Format::R8G8B8A8_UNORM,
+            samples: 1,
+            load_op: Clear,
+            store_op: Store,
+        },
+    },
+    pass: {
+        color: [color],
+        depth_stencil: {},
+    },
+)
+        .unwrap();
+
 }
