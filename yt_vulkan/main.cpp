d5b52f262b808ff70ebc52a182a23b5ec553ae69
Pedro Jorge <pmrjge@gmail.com>
2024-12-28 04:51:03 +0000

    template for the loop

--- yt_vulkan/main.cpp
+++ yt_vulkan/main.cpp
@@ -1,5 +1,48 @@
 #include <iostream>
 
-int main() {
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define WIDTH 1920
+#define HEIGHT 1080
+
+GLFWwindow* window = nullptr;
+
+void glfw_keycallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    if (!glfwVulkanSupported()) {
+        return 2;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+
+
+
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, glfw_keycallback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
---
+++ yt_vulkan/vk/setup_window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pmrj on 28-12-2024.
+//
+
+#include "setup_window.h"
