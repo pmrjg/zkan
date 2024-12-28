53d8a0c0c29557cbce2abd53a32cd2dc77da79af
Pedro Jorge <pmrjge@gmail.com>
2024-12-28 23:19:15 +0000

    Added precompiled headers

---
+++ simplex_vulkan/vk/deps/precomp.h
@@ -0,0 +1,12 @@
+//
+// Created by pmrj on 28-12-2024.
+//
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <gsl/gsl>
+#include <cstdint>
+#include <string>
+#include <string_view>
--- simplex_vulkan/vk/main.cpp
+++ simplex_vulkan/vk/main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <cstdlib>
-#include <cmath>
-#include <gsl/gsl>
-#include <cstdint>
 #include <GLFW/glfw3.h>
 
 #include "glfw/glfw_initialization.h"
@@ -11,7 +6,7 @@ std::int32_t main(std::int32_t argc, gsl::zstring* argv) {
 
     vkeng::GlfwInitialization _glfw;
 
-    gsl::not_null<GLFWwindow*> window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+    gsl::not_null window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
     gsl::final_action _cleanup_window([window] {glfwDestroyWindow(window);});
 
     while ( ! glfwWindowShouldClose(window) ) {
