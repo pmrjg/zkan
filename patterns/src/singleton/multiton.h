003c33e7ef16dedaf8e0588797d3c4064119ce90
Pedro Jorge <pmrjge@gmail.com>
2025-01-04 23:18:07 +0000

    Approach to the adapter pattern

--- patterns/src/singleton/multiton.h
+++ patterns/src/singleton/multiton.h
@@ -3,3 +3,34 @@
 //
 
 #pragma once
+#include <map>
+#include <memory>
+#include <string>
+
+enum class Importance {
+    primary,
+    secondary,
+    tertiary
+};
+
+template <typename T, typename Key = std::string>
+class Multiton {
+public:
+    static std::shared_ptr<T> get(const Key& key) {
+        if (const auto it = instances.find(key); it != instances.end()) {
+            return it->second;
+        }
+
+        auto instance = std::make_shared<T>();
+        instances[key] = instance;
+        return instance;
+    }
+protected:
+    Multiton() = default;
+    virtual ~Multiton() = default;
+private:
+    static std::map<Key, std::shared_ptr<T>> instances;
+};
+
+template <typename T, typename Key>
+std::map<Key, std::shared_ptr<T>> Multiton<T, Key>::instances;
---
+++ patterns/src/structure/adapter/example.h
@@ -0,0 +1,5 @@
+//
+// Created by Pedro Jorge on 04-01-2025.
+//
+
+#pragma once
